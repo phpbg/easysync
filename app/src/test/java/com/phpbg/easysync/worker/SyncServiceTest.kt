30293bbf3505786dc1ead5c1060a3a8270b44442
Samuel CHEMLA <samuel.chemla@orange.com>
2026-03-29 21:40:16 +0200

    Add unit tests

--- app/src/test/java/com/phpbg/easysync/worker/SyncServiceTest.kt
+++ app/src/test/java/com/phpbg/easysync/worker/SyncServiceTest.kt
@@ -28,11 +28,13 @@ import android.content.Context
 import android.util.Log
 import com.phpbg.easysync.Permissions
 import com.phpbg.easysync.dav.NotFoundExeption
+import com.phpbg.easysync.dav.RootPath
 import com.phpbg.easysync.dav.WebDavService
 import com.phpbg.easysync.db.ErrorDao
 import com.phpbg.easysync.db.FileDao
 import com.phpbg.easysync.mediastore.MediaStoreFile
 import com.phpbg.easysync.mediastore.MediaStoreService
+import com.phpbg.easysync.settings.ConflictStrategy
 import com.phpbg.easysync.settings.Settings
 import com.phpbg.easysync.settings.SettingsDataStore
 import kotlinx.coroutines.test.runTest
@@ -282,6 +284,151 @@ class SyncServiceTest {
 
         verify(webDavService).put(any(), any())
         verify(fileDao).updateFile(any())
+    }
+
+    @Test
+    fun `resyncFile should upload when conflict and strategy is KEEP_LOCAL`() = runTest {
+        val oldDate = Instant.now().minusSeconds(200)
+        val localDate = Instant.now().minusSeconds(100)
+        val remoteDate = Instant.now()
+        val dbFile = com.phpbg.easysync.db.File(
+            pathname = "/DCIM/photo.jpg",
+            localPathname = "/storage/emulated/0/DCIM/photo.jpg",
+            id = 1L,
+            etag = "old_etag",
+            localDateChanged = oldDate,
+            remoteDateChanged = oldDate,
+            isCollection = false
+        )
+        val mediaStoreFile = MediaStoreFile(
+            id = 1L,
+            displayName = "photo.jpg",
+            relativePath = "DCIM/",
+            absolutePath = "/storage/emulated/0/DCIM/photo.jpg",
+            dateModified = localDate,
+            isTrashed = false,
+        )
+        val remoteResource = com.phpbg.easysync.dav.Resource(
+            rootPath = mock(),
+            href = "/remote.php/dav/files/user/DCIM/photo.jpg",
+            creationdate = null,
+            getlastmodified = remoteDate,
+            isCollection = false,
+            getetag = "new_etag",
+            getcontentlength = "200",
+            getcontenttype = "image/jpeg"
+        )
+        val settings =
+            Settings(pathExclusions = emptySet(), conflictStrategy = ConflictStrategy.KEEP_LOCAL)
+        whenever(settingsDatastore.getSettings()).doReturn(settings)
+        whenever(webDavService.getPropertiesFromParentCache(any())).doReturn(remoteResource)
+        whenever(webDavService.getProperties(any<com.phpbg.easysync.dav.File>())).doReturn(
+            remoteResource
+        )
+        whenever(mediaStoreService.getById(1L)).doReturn(mediaStoreFile)
+        whenever(fileDao.findByName(dbFile.pathname)).doReturn(dbFile)
+
+        syncService.resyncFile(dbFile.pathname)
+
+        verify(webDavService).put(any(), any())
+        verify(fileDao).updateFile(any())
+    }
+
+    @Test
+    fun `resyncFile should download when conflict and strategy is KEEP_REMOTE`() = runTest {
+        val oldDate = Instant.now().minusSeconds(200)
+        val localDate = Instant.now().minusSeconds(100)
+        val remoteDate = Instant.now()
+        val dbFile = com.phpbg.easysync.db.File(
+            pathname = "/DCIM/photo.jpg",
+            localPathname = "/storage/emulated/0/DCIM/photo.jpg",
+            id = 1L,
+            etag = "old_etag",
+            localDateChanged = oldDate,
+            remoteDateChanged = oldDate,
+            isCollection = false
+        )
+        val mediaStoreFile = MediaStoreFile(
+            id = 1L,
+            displayName = "photo.jpg",
+            relativePath = "DCIM/",
+            absolutePath = "/storage/emulated/0/DCIM/photo.jpg",
+            dateModified = localDate,
+            isTrashed = false,
+        )
+        val remoteResource = com.phpbg.easysync.dav.Resource(
+            rootPath = RootPath("http://foo/remote.php/dav/files/user"),
+            href = "/remote.php/dav/files/user/DCIM/photo.jpg",
+            creationdate = null,
+            getlastmodified = null,
+            isCollection = false,
+            getetag = "new_etag",
+            getcontentlength = "200",
+            getcontenttype = "image/jpeg"
+        )
+        val settings =
+            Settings(pathExclusions = emptySet(), conflictStrategy = ConflictStrategy.KEEP_REMOTE)
+        whenever(settingsDatastore.getSettings()).doReturn(settings)
+        whenever(webDavService.getPropertiesFromParentCache(any())).doReturn(remoteResource)
+
+        whenever(mediaStoreService.getById(1L)).doReturn(mediaStoreFile)
+        whenever(fileDao.findByName(dbFile.pathname)).doReturn(dbFile)
+
+        val updatedMediaStoreFile = mediaStoreFile.copy(dateModified = remoteDate)
+        whenever(mediaStoreService.notifyUpdatedFile(any())).doReturn(mock())
+        whenever(mediaStoreService.getOneByUri(any())).doReturn(updatedMediaStoreFile)
+
+        mockStatic(java.nio.file.Files::class.java).use {
+            syncService.resyncFile(dbFile.pathname)
+        }
+
+        verify(webDavService).download(any(), any())
+        verify(fileDao).updateFile(any())
+    }
+
+    @Test
+    fun `resyncFile should do nothing when conflict and strategy is IGNORE`() = runTest {
+        val oldDate = Instant.now().minusSeconds(200)
+        val localDate = Instant.now().minusSeconds(100)
+        val remoteDate = Instant.now()
+        val dbFile = com.phpbg.easysync.db.File(
+            pathname = "/DCIM/photo.jpg",
+            localPathname = "/storage/emulated/0/DCIM/photo.jpg",
+            id = 1L,
+            etag = "old_etag",
+            localDateChanged = oldDate,
+            remoteDateChanged = oldDate,
+            isCollection = false
+        )
+        val mediaStoreFile = MediaStoreFile(
+            id = 1L,
+            displayName = "photo.jpg",
+            relativePath = "DCIM/",
+            absolutePath = "/storage/emulated/0/DCIM/photo.jpg",
+            dateModified = localDate,
+            isTrashed = false,
+        )
+        val remoteResource = com.phpbg.easysync.dav.Resource(
+            rootPath = mock(),
+            href = "/remote.php/dav/files/user/DCIM/photo.jpg",
+            creationdate = null,
+            getlastmodified = remoteDate,
+            isCollection = false,
+            getetag = "new_etag",
+            getcontentlength = "200",
+            getcontenttype = "image/jpeg"
+        )
+        val settings =
+            Settings(pathExclusions = emptySet(), conflictStrategy = ConflictStrategy.IGNORE)
+        whenever(settingsDatastore.getSettings()).doReturn(settings)
+        whenever(webDavService.getPropertiesFromParentCache(any())).doReturn(remoteResource)
+        whenever(mediaStoreService.getById(1L)).doReturn(mediaStoreFile)
+        whenever(fileDao.findByName(dbFile.pathname)).doReturn(dbFile)
+
+        syncService.resyncFile(dbFile.pathname)
+
+        verify(webDavService, never()).put(any(), any())
+        verify(webDavService, never()).download(any(), any())
     }
 
     @Test
